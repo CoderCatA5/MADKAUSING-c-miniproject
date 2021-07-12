1fa8b83d3460ec037c6ff4e42c5e37c0de6f40fe
CoderCatA5 <76586285+CoderCatA5@users.noreply.github.com>
2021-07-12 13:58:29 +0530

    ayushmaans edit

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@ int main()
     //initializing game window
     Game_Window = 2;
     //3 for pong
+    InitWindow(WINWIDTH, WINHEIGHT, "MadKauSing");
 
     //initializing elements
     //player1
@@ -76,11 +77,6 @@ int main()
 
     //initializing for tictactoe
 
-    const int WindowWidth = 1600;
-    const int WindowHeight = 900;
-
-    
-
     Image X = LoadImage("resources/X logo.png");
     ImageResize(&X, (float)X.width / 5, (float)X.height / 5);
     Image O = LoadImage("resources/O logo.png");
@@ -124,53 +120,67 @@ int main()
     g.game_end = false;
     for (int i = 0; i < 9; i++)
         g.pos[i] = 0;
-    InitWindow(WINWIDTH, WINHEIGHT, "PongVer3");
+
     SetTargetFPS(240);
     //mainloop which updates every
     while (!WindowShouldClose())
     {
-        mousePoint = GetMousePosition();
-        // if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
-        //     printf("%f %f\n",mousePoint.x,mousePoint.y);
-        //     // printf("%d\n",g.round);
-        // }
+        switch (Game_Window)
+        {
+            
+        case 3:
+            Draw_Pong(&player1, &player2, red1, red2, blue1, blue2, &ball, bricks);
+            break;
 
-        BeginDrawing();
-        ClearBackground(BLACK);
-        if (!check_win_condition(&g) && g.round < 9)
-        {
-            check_button(b, &g, mousePoint, Cross, Nought);
-            //Draws a grid of red lines marking the board
-            make_grid(b, WindowWidth, WindowHeight, grid_texture);
-            switch (g.round % 2)
+        case 2:
+            mousePoint = GetMousePosition();
+            // if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
+            //     printf("%f %f\n",mousePoint.x,mousePoint.y);
+            //     // printf("%d\n",g.round);
+            // }
+
+            BeginDrawing();
+            ClearBackground(BLACK);
+            if (!check_win_condition(&g) && g.round < 9)
             {
-            case 0:
-                DrawText("Player 1's turn", 300, 200, 30, BLUE);
-                break;
-            case 1:
-                DrawText("Player 2's turn", 1100, 200, 30, GREEN);
-                break;
+                check_button(b, &g, mousePoint, Cross, Nought);
+                //Draws a grid of red lines marking the board
+                make_grid(b, WINWIDTH, WINHEIGHT, grid_texture);
+                switch (g.round % 2)
+                {
+                case 0:
+                    DrawText("Player 1's turn", 300, 200, 30, BLUE);
+                    break;
+                case 1:
+                    DrawText("Player 2's turn", 1100, 200, 30, GREEN);
+                    break;
+                }
+                // check_win_condition(&g);
+                //if(check_win_condition(&g)) break;
             }
-            // check_win_condition(&g);
-            //if(check_win_condition(&g)) break;
-        }
-        else
-        {
-            switch (g.winner)
+            else
             {
-            case 0:
-                DrawText("It's a draw.", 700, 450, 30, YELLOW);
-                break;
-            case 1:
-                DrawText("Player 1 wins.", 700, 450, 30, BLUE);
-                break;
-            case 2:
-                DrawText("Player 2 wins.", 700, 450, 30, GREEN);
-                break;
+                switch (g.winner)
+                {
+                case 0:
+                    DrawText("It's a draw.", 700, 450, 30, YELLOW);
+                    break;
+                case 1:
+                    DrawText("Player 1 wins.", 700, 450, 30, BLUE);
+                    break;
+                case 2:
+                    DrawText("Player 2 wins.", 700, 450, 30, GREEN);
+                    break;
+                }
             }
+            EndDrawing();
+            break;
         }
-        EndDrawing();
     }
+    UnloadTexture(Cross);
+    UnloadTexture(Nought);
+    UnloadTexture(grid_texture);
+    UnloadTexture(blank);
 
     return 0;
 }
