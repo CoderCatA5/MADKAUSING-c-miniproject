b39a3b6c201d1a9f3ab2073204b60518b8bf7ab7
avijit <pandaavijit02@gmail.com>
2021-11-11 21:28:28 +0530

    delete

--- src/something.c
+++ src/something.c
@@ -1,10 +1,9 @@
 //first of all this theme is shit cuz u cant see commments
 //this was written by avijit
 
+///xlzkjxjzkjx
 
-
-
-///some thing 
+///some thing
 //dsihsjhdsjhd
 ///dasjshdajhkj
 ///dasljhdaskjh/dasjhgdhasjkgd
